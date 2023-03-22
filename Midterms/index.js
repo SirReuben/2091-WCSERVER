f72d3bcb70fbe6705cdbbf124e9f102d7832b13f
Reuben Rob <98816708+SirReuben@users.noreply.github.com>
2023-03-22 16:24:50 +0800

    Add files via upload

--- Midterms/index.js
+++ Midterms/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
-const port = 3111;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Welcome to the Express starter template for Stackblitz!');
+const dishes = [
+  {
+    type: 'Sisig',
+    province: 'Pampanga',
+    price: '220',
+  },
+  {
+    type: 'Salpicao',
+    province: 'Quezon',
+    price: '180',
+  },
+  {
+    type: 'Bagnet',
+    province: 'Ilocos',
+    price: '370',
+  },
+];
+
+app.get('/dishes', (req, res) => {
+  res.send(dishes);
 });
 
-app.listen(port, () => {
-  console.log(`App is live at http://localhost:${port}`);
+app.get('/dishes/:type', (req, res) => {
+  const dishes = dishes.find((h) => h.type === parseInt(req.params.type));
+  if (!dishes) return res.status(404).send('Record not found');
+  res.send(dishes);
 });
+
+app.listen(3000, () => console.log('listening on port 3000'));
