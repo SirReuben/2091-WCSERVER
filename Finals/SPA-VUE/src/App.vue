bf3b219bbef9bfaff80e1ad17a76fc9b336fdd36
SirReuben <98816708+SirReuben@users.noreply.github.com>
2023-04-22 20:19:08 +0800

    11 files added, 6 deleted

---
+++ Finals/SPA-VUE/src/App.vue
@@ -0,0 +1,362 @@
+<template>
+  <div id="app">
+    <link
+      href="https://fonts.googleapis.com/css?family=Lato:400,300,700"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <div class="container">
+      <div class="header">
+        <div class="full-name">
+          <span class="first-name">Reuben Rob</span>
+          <span class="last-name">Sibal</span>
+        </div>
+        <div class="contact-info">
+          <span class="email">Email: </span>
+          <span class="email-val">sibalreuben@gmail.com</span>
+          <span class="separator"></span>
+          <span class="phone">Phone: </span>
+          <span class="phone-val">0951-420-9285</span>
+        </div>
+
+        <div class="about">
+          <span class="position">OBJECTIVE: </span>
+          <span class="desc">
+            I am seeking a competitive and challenging environment where I can
+            exploit my potential and can deliver the best performance by
+            utilizing my Information and Communication Technology skills and
+            ability to work well with people.
+          </span>
+        </div>
+      </div>
+      <div class="details">
+        <div class="section">
+          <div class="section__title">Experience</div>
+          <div class="section__list">
+            <div class="section__list-item">
+              <div class="left">
+                <div class="name">City Employment Services Office (CESO)</div>
+                <div class="addr">Barangay Hall of Poblacion</div>
+                <div class="duration">2019 April to 2019 May</div>
+              </div>
+              <div class="right">
+                <div class="name">Working Student</div>
+                <div class="desc">Encoder, Census Attendant, and Helper</div>
+              </div>
+            </div>
+            <div class="section__list-item">
+              <div class="left">
+                <div class="name">City Employment Services Office (CESO)</div>
+                <div class="addr">CESO Office</div>
+                <div class="duration">2018 April to 2018 May</div>
+              </div>
+              <div class="right">
+                <div class="name">Working Student</div>
+                <div class="desc">Encoder, and Helper</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="section">
+          <div class="section__title">References</div>
+          <div class="section__list">
+            <div class="section__list-item">
+              <div class="text">
+                Judy David<br />Employee<br />City Employment Services Office<br />09284392030
+              </div>
+            </div>
+
+            <div class="section__list-item">
+              <div class="section__title">Languages</div>
+              <div class="text"><a>English, Tagalog, Kapampangan.</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="section">
+          <div class="section__title">Skills</div>
+          <div class="skills">
+            <div class="skills__item">
+              <div class="left"><div class="name">Python</div></div>
+              <div class="right">
+                <input id="ck1" type="checkbox" checked />
+
+                <label for="ck1"></label>
+                <input id="ck2" type="checkbox" checked />
+
+                <label for="ck2"></label>
+                <input id="ck3" type="checkbox" checked />
+
+                <label for="ck3"></label>
+                <input id="ck4" type="checkbox" checked />
+                <label for="ck4"></label>
+                <input id="ck5" type="checkbox" />
+                <label for="ck5"></label>
+              </div>
+            </div>
+          </div>
+          <div class="skills__item">
+            <div class="left"><div class="name">HTML/CSS</div></div>
+            <div class="right">
+              <input id="ck1" type="checkbox" checked />
+
+              <label for="ck1"></label>
+              <input id="ck2" type="checkbox" checked />
+
+              <label for="ck2"></label>
+              <input id="ck3" type="checkbox" checked />
+
+              <label for="ck3"></label>
+              <input id="ck4" type="checkbox" checked />
+              <label for="ck4"></label>
+              <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+            </div>
+          </div>
+
+          <div class="skills__item">
+            <div class="left"><div class="name">PHP</div></div>
+            <div class="right">
+              <input id="ck1" type="checkbox" checked />
+
+              <label for="ck1"></label>
+              <input id="ck2" type="checkbox" checked />
+
+              <label for="ck2"></label>
+              <input id="ck3" type="checkbox" checked />
+
+              <label for="ck3"></label>
+              <input id="ck4" type="checkbox" checked />
+              <label for="ck4"></label>
+              <input id="ck5" type="checkbox" />
+              <label for="ck5"></label>
+            </div>
+
+            <div class="skills__item">
+              <div class="left"><div class="name">NodeJS</div></div>
+              <div class="right">
+                <input id="ck1" type="checkbox" checked />
+
+                <label for="ck1"></label>
+                <input id="ck2" type="checkbox" checked />
+
+                <label for="ck2"></label>
+                <input id="ck3" type="checkbox" checked />
+
+                <label for="ck3"></label>
+                <input id="ck4" type="checkbox" />
+                <label for="ck4"></label>
+                <input id="ck5" type="checkbox" />
+                <label for="ck5"></label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="section">
+          <div class="section__title">Education</div>
+          <div class="section__list">
+            <div class="section__list-item">
+              <div class="left">
+                <div class="name">TALIMUNDOK ELEMENTARY SCHOOL</div>
+                <div class="addr">Dau, Mabalacat City, Pampanga</div>
+                <div class="duration">2009 - 2015</div>
+              </div>
+              <div class="right">
+                <div class="name">Intermediate</div>
+              </div>
+            </div>
+            <div class="section__list-item">
+              <div class="left">
+                <div class="name">MABALACAT NATIONAL HIGH SCHOOL</div>
+                <div class="addr">Dau, Mabalacat City, Pampanga</div>
+                <div class="duration">2015 - 2019</div>
+              </div>
+              <div class="right">
+                <div class="name">Secondary</div>
+              </div>
+            </div>
+            <div class="section__list-item">
+              <div class="left">
+                <div class="name">SYSTEMS PLUS COLLEGE FOUNDATION</div>
+                <div class="addr">Balibago, Angeles City, Pampanga</div>
+                <div class="duration">2019 - 2021</div>
+              </div>
+              <div class="right">
+                <div class="name">Senior High School</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="section">
+          <div class="section__title">Hobbies</div>
+          <div class="section__list">
+            <div class="section__list-item">
+              Basketball, Cycling, Volleyball.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  height: 100%;
+}
+
+body {
+  min-height: 100%;
+  background: #eee;
+  font-family: 'Lato', sans-serif;
+  font-weight: 400;
+  color: #222;
+  font-size: 14px;
+  line-height: 26px;
+  padding-bottom: 50px;
+}
+
+.container {
+  max-width: 700px;
+  background: #fff;
+  margin: 0px auto 0px;
+  box-shadow: 1px 1px 2px #dad7d7;
+  border-radius: 3px;
+  padding: 40px;
+  margin-top: 50px;
+}
+
+.header {
+  margin-bottom: 30px;
+}
+.header .full-name {
+  font-size: 40px;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+}
+.header .first-name {
+  font-weight: 700;
+}
+.header .last-name {
+  font-weight: 300;
+}
+.header .contact-info {
+  margin-bottom: 20px;
+}
+.header .email,
+.header .phone {
+  color: #999;
+  font-weight: 300;
+}
+.header .separator {
+  height: 10px;
+  display: inline-block;
+  border-left: 2px solid #999;
+  margin: 0px 10px;
+}
+.header .position {
+  font-weight: bold;
+  display: inline-block;
+  margin-right: 10px;
+  text-decoration: underline;
+}
+
+.details {
+  line-height: 20px;
+}
+.details .section {
+  margin-bottom: 40px;
+}
+.details .section:last-of-type {
+  margin-bottom: 0px;
+}
+.details .section__title {
+  letter-spacing: 2px;
+  color: #54afe4;
+  font-weight: bold;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+}
+.details .section__list-item {
+  margin-bottom: 40px;
+}
+.details .section__list-item:last-of-type {
+  margin-bottom: 0;
+}
+.details .left,
+.details .right {
+  vertical-align: top;
+  display: inline-block;
+}
+.details .left {
+  width: 60%;
+}
+.details .right {
+  tex-align: right;
+  width: 39%;
+}
+.details .name {
+  font-weight: bold;
+}
+.details a {
+  text-decoration: none;
+  color: #000;
+  font-style: italic;
+}
+.details a:hover {
+  text-decoration: underline;
+  color: #000;
+}
+.details .skills__item {
+  margin-bottom: 10px;
+}
+.details .skills__item .right input {
+  display: none;
+}
+.details .skills__item .right label {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background: #c3def3;
+  border-radius: 20px;
+  margin-right: 3px;
+}
+.details .skills__item .right input:checked + label {
+  background: #79a9ce;
+}
+</style>
